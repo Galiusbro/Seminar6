231c5ead8be93608567fc1a7e5f8c06487a8e072
Galprim <galprimulus@gmail.com>
2022-05-10 12:03:58 +0300

    add changes Task 43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -4,42 +4,31 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 
 
-Console.Write("Для уравнения y = k1 * x + b1 введите k1 и b1 через пробел: ");
-String numbers1 = Console.ReadLine();
+Console.WriteLine("Введите коэффициенты первой прямой:");
+Console.Write("k1 = ");
+float k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b1 = ");
+float b1 = Convert.ToInt32(Console.ReadLine());
 
-string[] massString1 = numbers1.Split();
-int[] line1 = StringToInt(massString1);
-
-Console.Write("Для уравнения y = k2 * x + b2 введите k2 и b2 через пробел: ");
-String numbers2 = Console.ReadLine();
-
-string[] massString2 = numbers2.Split();
-int[] line2 = StringToInt(massString2);
-
-
+Console.WriteLine("Введите коэффициенты второй прямой:");
+Console.Write("k2 = ");
+float k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2 = ");
+float b2 = Convert.ToInt32(Console.ReadLine());
 
 
-int[] StringToInt(string[] array)
-{
-    int[] massInt  = new int[array.Length];
-    for (int i = 0; i < massInt.Length; i++)
-    {
-        massInt[i] = Convert.ToInt32(array[i]);
-    }
-    return massInt;
-}
 
-void IntersectionPoint(int[] line1, int[] line2)
+void IntersectionPoint(float k1, float b1, float k2, float b2)
 {
     float x = 0, y = 0;
-    if (line1[0] == line2[0] && line1[1] == line2[1]) Console.WriteLine("Прямые совпадают");
-    else if (line1[0] == line2[0] && line1[1] != line2[1]) Console.WriteLine("Прямые параллельны");
+    if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают");
+    else if (k1 == k2 && b1 != b2) Console.WriteLine("Прямые параллельны");
     else 
     {
-        x = (line1[1] - line2[1]) / (line1[0] - line2[0]);
-        y = (line2[0] * line1[1] - line1[0] * line2[1]);
+        x = (b1 - b2) / (k2 - k1);
+        y = (k2 * b1 - k1 * b2) / (k2 - k1);
     }
-    Console.WriteLine($"x = {x}, y = {y}");
+    Console.WriteLine($"x = {x:f1}, y = {y:f1}");
 }
 
-IntersectionPoint(line1, line2);
+IntersectionPoint(k1, b1, k2, b2);
