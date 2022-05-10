94f30e5a378404bfa761d2006524e1362f61b42f
Galprim <galprimulus@gmail.com>
2022-05-10 10:23:19 +0300

    add Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Task 41
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Write("Введите любые целые числа через пробел: ");
+String numbers  = Console.ReadLine();
+
+string[] massString = numbers.Split();
+int[] massInt  = new int[massString.Length];
+for (int i = 0; i < massInt.Length; i++)
+{
+    massInt[i] = Convert.ToInt32(massString[i]);
+}
+
+void AboveZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    Console.WriteLine($"Вы ввели {count} чисел больше нуля");
+}
+
+AboveZero(massInt);
